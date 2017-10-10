d2c588b4fca214dd1ddd16421509ac7fe5b710d1
Thorsten Zoerner <me@thorsten-zoerner.com>
2017-10-11 01:56:19 +0200

    Updated Linkage

--- index.js
+++ index.js
@@ -41,9 +41,11 @@ ipfs.on('ready', () => {
 	
 	ipfs.swarm.connect("/ip4/108.61.210.201/tcp/4002/ipfs/QmPSrr2c4bWCZ7R6N2tGR8Pj3CK7uWVtANtEZSb8Tptfsv");
 	ipfs.pubsub.subscribe("stromdao-query", receiveMsg);
+	/*
 	setInterval(function() {	
 	ipfs.pubsub.publish("stromdao-query",new Buffer("0x9d3544a49d0940a7923b7c109165d246e62f20f7d628edf3e3cb0e1184121d06"),(err) => { console.log("SEND"); });
 	},5000);
+	*/
 	console.log("Node Ready");
 })
 
